2866897dca49eed27e484b6064e6d4ccd328db95
Your GitHub Username <Your GitHub Email>
2024-07-15 16:40:50 +0700

    first commit

---
+++ duan20/client/layout/footer.php
@@ -0,0 +1,174 @@
+
+<!-- Phần Banner thứ 2 -->
+<div class="none">
+<section class="banner02" style="background-color: #eae2d8;">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-md-6" id="pading" style="     padding-left: 188px; ">
+                <div class="banner-content" style="text-align: center;padding: 32px;">
+                    <h1>Giảm giá 50%</h1>
+                    <h4>Lenovo ThinkBook 14 G3 ACL R3 5300U (21A200RWVN)</h4>
+                    <del>15.990.000₫</del>
+                    <h4>7.990.000₫</h4>
+                    <div class="slider-btn btn-hover" style="margin-top: 34px; ">
+                        <a class="animated bo" href="index.php?act=sanphamct&idsp=20" style="     margin-top: 20px;     border: 1px solid;     padding: 8px; ">MUA NGAY</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="ban-img">
+                    <img src="upload/anh-trong-suot-3.png" alt="" style="width: 500px;height: 310px;margin: 54px;">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Hết phần banner thứ 2 -->
+<div class="blog-area pb-55">
+    <div class="container">
+        <div class="section-title text-center mb-55 mt-5">
+            <h2>Sản phẩm nổi bật</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="blog-wrap mb-30 scroll-zoom">
+                    <div class="blog-img">
+                        <a href="index.php?act=sanphamct&idsp=13"><img src="upload/a6.jpg" alt=""></a>
+                        <span class="purple">-20%</span>
+                    </div>
+                    <div class="blog-content-wrap">
+                        <div class="blog-content text-center">
+                            <h3><a href="index.php?act=sanphamct&idsp=13">HP ProOne 440 G9 AIO i7 <br> 12700T 23.8 inch</a></h3>
+                            <span> <a href="index.php?act=sanphamct&idsp=13">24.390.000₫</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="blog-wrap mb-30 scroll-zoom">
+                    <div class="blog-img">
+                        <a href="index.php?act=sanphamct&idsp=12"><img src="upload/a8.jpg" alt=""></a>
+                        <span class="pink">-20%</span>
+                    </div>
+                    <div class="blog-content-wrap">
+                        <div class="blog-content text-center">
+                            <h3><a href="index.php?act=sanphamct&idsp=12">HP AIO ProOne 240 G9 i3 <br> 1215U 23.8 inch</a></h3>
+                            <span><a href="index.php?act=sanphamct&idsp=12">15.390.000₫</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="blog-wrap mb-30 scroll-zoom">
+                    <div class="blog-img">
+                        <a href="index.php?act=sanphamct&idsp=15"><img src="upload/a10.jpg" alt=""></a>
+                        <span class="purple">-30%</span>
+                    </div>
+                    <div class="blog-content-wrap">
+                        <div class="blog-content text-center">
+                            <h3><a href="index.php?act=sanphamct&idsp=15">Asus Vivobook X415EA i3 5300U (21A200RWVN)<br></a></h3>
+                            <span><a href="index.php?act=sanphamct&idsp=15">15.060.000₫</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="footer-area bg-gray pt-100 pb-70">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-md-4 col-sm-4">
+                <div class="copyright mb-30">
+                    <div class="footer-logo">
+                        <a href="index.php">
+                            <img alt="" src="assets/img/logo/laptech.png" style="     width: 100px; ">
+                        </a>
+                    </div>
+                    <p>© 2023 <a href="#">LAPDTECH</a>.<br> All Rights Reserved</p>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-4">
+                <div class="footer-widget mb-30 ml-30">
+                    <div class="footer-title">
+                        <h3>GIỚI THIỆU</h3>
+                    </div>
+                    <div class="footer-list">
+                        <ul>
+                            <li><a href="about.html">GIỚI THIỆU</a></li>
+                            <li><a href="#">Vị trí cửa hàng</a></li>
+                            <li><a href="contact.html">Liên hệ</a></li>
+                            <li><a href="#">Theo dõi đơn hàng</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-4">
+                <div class="footer-widget mb-30 ml-50">
+                    <div class="footer-title">
+                        <h3>LIÊN KẾT HỮU ÍCH</h3>
+                    </div>
+                    <div class="footer-list">
+                        <ul>
+                            <li><a href="#">Hỗ Trợ Kỹ Thuật</a></li>
+                            <li><a href="#">Chính Sách Đổi Trả</a></li>
+                            <li><a href="#">Chính Sách Bảo Mật</a></li>
+                            <li><a href="#">Câu Hỏi Thường Gặp</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="footer-widget mb-30 ml-75">
+                    <div class="footer-title">
+                        <h3>THEO DÕI CHÚNG TÔI</h3>
+                    </div>
+                    <div class="footer-list">
+                        <ul>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">Youtube</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="footer-widget mb-30 ml-70">
+                    <div class="footer-title">
+                        <h3>ĐĂNG KÝ</h3>
+                    </div>
+                    <div class="subscribe-style">
+                        <p>Đăng Ký Email để nhận thông tin về các sản phẩm mới của chúng tôi</p>
+                        <div id="mc_embed_signup" class="subscribe-form">
+                            <form id="mc-embedded-subscribe-form" class="validate" novalidate="" target="_blank" name="mc-embedded-subscribe-form" method="post" action="https://devitems.us11.list-manage.com/subscribe/post?u=6bbb9b6f5827bd842d9640c82&amp;id=05d85f18ef">
+                                <div id="mc_embed_signup_scroll" class="mc-form">
+                                    <input class="email" type="email" required="" placeholder="Nhập email..." name="EMAIL" value="">
+                                    <div class="mc-news" aria-hidden="true">
+                                        <input type="text" value="" tabindex="-1" name="b_6bbb9b6f5827bd842d9640c82_05d85f18ef">
+                                    </div>
+                                    <div class="clear">
+                                        <input id="mc-embedded-subscribe" class="button" type="submit" name="subscribe" value="Đăng ký">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+<script src="assets/js/vendor/modernizr-3.11.7.min.js"></script>
+<script src="assets/js/vendor/jquery-v3.6.0.min.js"></script>
+<script src="assets/js/vendor/jquery-migrate-v3.3.2.min.js"></script>
+<script src="assets/js/vendor/popper.min.js"></script>
+<script src="assets/js/vendor/bootstrap.min.js"></script>
+<script src="assets/js/plugins.js"></script>
+<!-- Ajax Mail -->
+<script src="assets/js/ajax-mail.js"></script>
+<!-- Main JS -->
+<script src="assets/js/main.js"></script>
+</body>
+</html>
---
+++ duan20/client/layout/slide.php
@@ -0,0 +1,96 @@
+<div class="none">
+    <div class="slider-area">
+        <div class="slider-active owl-carousel nav-style-1 owl-dot-none">
+            <div class="single-slider slider-height-1 bg-purple">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6">
+                            <div class="slider-content slider-animated-1">
+                                <h3 class="animated">Sản phẩm HOT 2023</h3>
+                                <h1 class="animated">Laptop Dell Vostro<br> 3400 </h1>
+                                <div class="slider-btn btn-hover">
+                                    <a class="animated" href="shop.html">MUA NGAY</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6" style="     margin-top: 37px; ">
+                            <div class="slider-single-img slider-animated-1">
+                                <img class="animated" src="assets/img/slider/single-slide-1.png" alt="" style="    margin: 0 33px 76px; ">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slider slider-height-1 bg-purple">
+                <div class="container">
+                    <div class="row" style="     margin: -70px -80px 101px -73px; ">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6">
+                            <div class="slider-content slider-animated-1" style="padding: 0px;">
+                                <h3 class="animated">Sản phẩm HOT</h3>
+                                <h1 class="animated">Lenovo ThinkBook<br> 14 G3 ACL</h1>
+                                <div class="slider-btn btn-hover">
+                                    <a class="animated" href="shop.html">MUA NGAY</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6">
+                            <div class="slider-single-img slider-animated-1">
+                                <img class="animated" src="assets/img/slider/single-slide-hm1-2.png" alt="" style="width: 600px;height: 397px;margin-left: 101px;">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="suppoer-area pt-100 pb-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="support-wrap mb-30 support-1">
+                        <div class="support-icon">
+                            <img class="animated" src="assets/img/icon-img/support-1.png" alt="">
+                        </div>
+                        <div class="support-content">
+                            <h5>Miễn Phí Vận Chuyển</h5>
+                            <p>Miễn phí vận chuyển khi mua hàng</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="support-wrap mb-30 support-2">
+                        <div class="support-icon">
+                            <img class="animated" src="assets/img/icon-img/support-2.png" alt="">
+                        </div>
+                        <div class="support-content">
+                            <h5>Hỗ Trợ 24/7</h5>
+                            <p>Hỗ trợ khách hàng 24/7</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="support-wrap mb-30 support-3">
+                        <div class="support-icon">
+                            <img class="animated" src="assets/img/icon-img/support-3.png" alt="">
+                        </div>
+                        <div class="support-content">
+                            <h5>Hoàn Tiền</h5>
+                            <p>Đảm bảo hoàn tiền nếu có vấn đề</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="support-wrap mb-30 support-4">
+                        <div class="support-icon">
+                            <img class="animated" src="assets/img/icon-img/support-4.png" alt="">
+                        </div>
+                        <div class="support-content">
+                            <h5>Giảm Giá Khi Đặt Hàng</h5>
+                            <p>Giảm giá cho các đơn hàng lớn</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
